856e87ca338adc2361cbf8b63e150b610be4edde
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2021-08-25 10:50:41 -0600

    Add node picking

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.5.20"
+    kotlin("jvm") version "1.5.30"
     id("org.openjfx.javafxplugin") version "0.0.10"
     id("maven-publish")
 }
--- src/main/kotlin/Nodes.kt
+++ src/main/kotlin/Nodes.kt
@@ -3,6 +3,7 @@
 package kfx
 
 import javafx.scene.Node
+import javafx.scene.Parent
 import javafx.scene.layout.Region
 import javafx.scene.shape.Rectangle
 
@@ -16,3 +17,48 @@ public inline fun Region.clipToBounds() {
 public inline fun Node.hasProperty(key: Any?): Boolean = hasProperties() && key in properties
 public inline fun Node.getProperty(key: Any?): Any? = if (hasProperties()) properties[key] else null
 public inline fun Node.removeProperty(key: Any?): Any? = if (hasProperties()) properties.remove(key) else null
+
+/**
+ * Returns the deepest node at ([sceneX], [sceneY]) that matches the conditions
+ *
+ * **[sceneX] and [sceneY] should be the root scene coordinates_**
+ *
+ * Conditions:
+ * - [ignoreMouseTransparent]: Skip nodes that are transparent to the mouse
+ * - [ignoreInvisible]: Skip nodes that are not visible
+ * - [ignoreUnmanaged]: Skip nodes that are not managed
+ * - [ignoreDisabled]: Skip nodes that are disabled
+ * - [isValid]: Any additional validation
+ *
+ * _Original code from [Jonathan Giles](http://fxexperience.com/2016/01/node-picking-in-javafx/)_
+ */
+public fun Node.pick(
+    sceneX: Double, sceneY: Double,
+    ignoreMouseTransparent: Boolean = true,
+    ignoreInvisible: Boolean = true,
+    ignoreUnmanaged: Boolean = false,
+    ignoreDisabled: Boolean = false,
+    isValid: (Node.() -> Boolean)? = null,
+): Node? {
+    if (
+        (!isVisible && ignoreInvisible) ||
+        (isMouseTransparent && ignoreMouseTransparent) ||
+        (!isManaged && ignoreUnmanaged) ||
+        (isDisabled && ignoreDisabled) ||
+        sceneToLocal(sceneX, sceneY) !in this ||
+        (isValid != null && !isValid())
+    ) return null
+    if (this is Parent) {
+        for (child in childrenUnmodifiable) {
+            return child?.pick(
+                sceneX, sceneY,
+                ignoreMouseTransparent,
+                ignoreInvisible,
+                ignoreUnmanaged,
+                ignoreDisabled,
+                isValid,
+            ) ?: continue
+        }
+    }
+    return this
+}
