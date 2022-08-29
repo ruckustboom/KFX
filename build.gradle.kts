8689afbd8b04566ceca65e0308d4a1c0c68c9c53
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2022-08-29 13:47:08 -0600

    Kotlin 1.7.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
     id("org.openjfx.javafxplugin") version "0.0.12"
     id("maven-publish")
 }
