ee8827e4aac267e53b3cc76801a21a7d1121a24c
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2022-09-30 09:28:23 -0600

    Add ability for TextFields to serve as an editor for arbitrary properties using a StringConverter
    gh-11

---
+++ src/main/kotlin/TextFields.kt
@@ -0,0 +1,65 @@
+package kfx
+
+import javafx.beans.property.*
+import javafx.scene.control.TextField
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
+import javafx.util.StringConverter
+
+public fun <T> TextField.editorFor(property: Property<T>, converter: StringConverter<T>) {
+    property.addAndRunListener { refresh(property, converter) }
+    focusedProperty().addListener { _, wasFocused, isFocused ->
+        if (!isFocused && wasFocused) update(property, converter)
+    }
+    addEventHandler(KeyEvent.KEY_PRESSED) {
+        when (it?.code) {
+            KeyCode.ENTER -> {
+                update(property, converter)
+                selectAll()
+                it.consume()
+            }
+
+            KeyCode.ESCAPE -> {
+                refresh(property, converter)
+                parent?.requestFocus()
+                it.consume()
+            }
+
+            else -> Unit
+        }
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+public fun TextField.editorFor(property: IntegerProperty, converter: StringConverter<Int>) {
+    editorFor(property as Property<Int>, converter)
+}
+
+@Suppress("UNCHECKED_CAST")
+public fun TextField.editorFor(property: LongProperty, converter: StringConverter<Long>) {
+    editorFor(property as Property<Long>, converter)
+}
+
+@Suppress("UNCHECKED_CAST")
+public fun TextField.editorFor(property: FloatProperty, converter: StringConverter<Float>) {
+    editorFor(property as Property<Float>, converter)
+}
+
+@Suppress("UNCHECKED_CAST")
+public fun TextField.editorFor(property: DoubleProperty, converter: StringConverter<Double>) {
+    editorFor(property as Property<Double>, converter)
+}
+
+private fun <T> TextField.update(property: Property<T>, converter: StringConverter<T>) {
+    try {
+        property.value = converter.fromString(text)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    } finally {
+        refresh(property, converter)
+    }
+}
+
+private fun <T> TextField.refresh(property: Property<T>, converter: StringConverter<T>) {
+    text = converter.toString(property.value)
+}
