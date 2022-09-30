17a867b8b73b54f92ac84126e463295d292877a9
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2022-09-30 09:55:13 -0600

    Add converters for numbers
    gh-12

--- src/main/kotlin/TextFields.kt
+++ src/main/kotlin/TextFields.kt
@@ -42,24 +42,52 @@ public fun <T> TextField.editorFor(
     }
 }
 
+public abstract class SimpleStringConverter<T> : StringConverter<T>() {
+    override fun toString(value: T): String = value.toString()
+}
+
+// Int
+
 @Suppress("UNCHECKED_CAST")
 public fun TextField.editorFor(property: IntegerProperty, converter: StringConverter<Int>) {
     editorFor(property as Property<Int>, converter)
 }
+
+public object IntConverter : SimpleStringConverter<Int>() {
+    override fun fromString(string: String): Int = string.toInt()
+}
+
+// Long
 
 @Suppress("UNCHECKED_CAST")
 public fun TextField.editorFor(property: LongProperty, converter: StringConverter<Long>) {
     editorFor(property as Property<Long>, converter)
 }
 
+public object LongConverter : SimpleStringConverter<Long>() {
+    override fun fromString(string: String): Long = string.toLong()
+}
+
+// Float
+
 @Suppress("UNCHECKED_CAST")
 public fun TextField.editorFor(property: FloatProperty, converter: StringConverter<Float>) {
     editorFor(property as Property<Float>, converter)
 }
 
+public object FloatConverter : SimpleStringConverter<Float>() {
+    override fun fromString(string: String): Float = string.toFloat()
+}
+
+// Double
+
 @Suppress("UNCHECKED_CAST")
 public fun TextField.editorFor(property: DoubleProperty, converter: StringConverter<Double>) {
     editorFor(property as Property<Double>, converter)
+}
+
+public object DoubleConverter : SimpleStringConverter<Double>() {
+    override fun fromString(string: String): Double = string.toDouble()
 }
 
 // Impl
