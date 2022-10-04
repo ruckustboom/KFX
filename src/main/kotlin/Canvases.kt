ff49cb52dbeeb94a266b05abda284a7949f86962
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2022-10-04 13:09:23 -0600

    Add canvas helpers

--- src/main/kotlin/Canvases.kt
+++ src/main/kotlin/Canvases.kt
@@ -4,15 +4,17 @@ import javafx.scene.canvas.Canvas
 import javafx.scene.canvas.GraphicsContext
 import javafx.scene.paint.Paint
 
-public fun Canvas.clear(fill: Paint? = null) {
-    with(graphicsContext2D) {
-        clearRect(0.0, 0.0, width, height)
-        if (fill != null) {
-            val p = this.fill
-            this.fill = fill
-            graphicsContext2D.fillRect(0.0, 0.0, width, height)
-            this.fill = p
-        }
+public val GraphicsContext.width: Double get() = canvas.width
+public val GraphicsContext.height: Double get() = canvas.height
+
+public fun Canvas.clear(fill: Paint? = null): Unit = graphicsContext2D.clear(fill)
+public fun GraphicsContext.clear(fill: Paint? = null) {
+    clearRect(0.0, 0.0, width, height)
+    if (fill != null) {
+        val p = this.fill
+        this.fill = fill
+        fillRect(0.0, 0.0, width, height)
+        this.fill = p
     }
 }
 
